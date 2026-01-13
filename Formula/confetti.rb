c6a6ae81753894f9b3c616678df889711ac09e3d
William Alexander <alexandw@wfu.edu>
2026-01-13 09:33:13 -0500

    Point formula at public release

--- Formula/confetti.rb
+++ Formula/confetti.rb
@@ -1,7 +1,7 @@
 class Confetti < Formula
   desc "One-shot macOS confetti overlay"
   homepage "https://github.com/wmalexander/Confetti-"
-  url "https://github.com/wmalexander/Confetti-/releases/download/v0.1.0/confetti-0.1.0.tar.gz"
+  url "https://github.com/wmalexander/homebrew-confetti/releases/download/v0.1.0/confetti-0.1.0.tar.gz"
   sha256 "2c9775b779e696afb51effe5cbcfe6de6c7be9c2721a2167d55d5cc08fa1ae37"
   license "MIT"
 
